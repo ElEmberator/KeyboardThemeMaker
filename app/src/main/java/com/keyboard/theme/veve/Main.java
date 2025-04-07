48531bf9522fe76efb220ad7a61904364ed27662
Ahmed ELEmberator <64263286+ElEmberator@users.noreply.github.com>
2025-04-07 03:15:37 +0200

    Add app/src/main/java/com/keyboard/theme/veve/Main.java

---
+++ app/src/main/java/com/keyboard/theme/veve/Main.java
@@ -0,0 +1,26 @@
+package com.keyboard.theme.veve;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+
+public class Main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        Button settingsButton = findViewById(R.id.settings_button);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Settings.ACTION_INPUT_METHOD_SETTINGS);
+                startActivity(intent);
+            }
+        });
+    }
+}
